617d3f2288a574feb40f02f18b0a82b47b6bae2e
SHEFFcode <sheff@sheffmachine.com>
2017-03-01 23:35:46 -0800

    produce binary search example

--- search.rb
+++ search.rb
@@ -2,7 +2,7 @@
 
 def largest_number(arr)
     largest_number = 0
-
+    
     arr.each do |i|
         largest_number = i if i > largest_number
     end
@@ -14,7 +14,26 @@ end
 
 #Binary search with a sorted array
 def binary_search (arr, target)
+    min_index = 0
+    max_index = arr.length - 1
+    
+    while (min_index <= max_index)
+
+        midpoint = (max_index + min_index) / 2
+        if (arr[midpoint] > target)
+            max_index = midpoint - 1
+        elsif (arr[midpoint] < target)
+            min_index = midpoint + 1
+        else
+            puts midpoint
+            return midpoint 
+        end
+        
+    end
+    
+    puts "element is not in array"
+    return -1
     
 end
 
-binary_search([1, 2, 3, 5, 20, 40], 5)
+binary_search([1, 2, 3, 5, 20, 40], 6)
