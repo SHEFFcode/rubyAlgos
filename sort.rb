834b5778ec99c625f2b9bba067debc77389d7392
SHEFFcode <sheff@sheffmachine.com>
2017-03-02 19:48:25 -0800

    merge sort

--- sort.rb
+++ sort.rb
@@ -20,6 +20,37 @@ def bubble_sort (arr)
     end
 end
 
+# arr = (0..10).to_a.shuffle
+# print arr
+# print bubble_sort(arr)
+
+#Merge Sort
+def merge_sort(arr)
+    return arr if arr.length == 1
+    midpoint = (arr.length - 1) / 2
+
+    arr_1 = merge_sort(arr[0..midpoint])
+    arr_2 = merge_sort(arr[(midpoint + 1)..(arr.length - 1)])
+
+    return merge(arr_1, arr_2)
+end
+
+def merge (arr_1, arr_2)
+    arr = []
+    while arr_1.length > 0 && arr_2.length > 0
+        if arr_1[0] < arr_2[0]
+            arr.push(arr_1.shift)
+        else 
+            arr.push(arr_2.shift)
+        end
+    end
+    arr += arr_1 if arr_1.length > 0
+    arr += arr_2 if arr_2.length > 0
+
+    return arr
+end
+
+
 arr = (0..10).to_a.shuffle
 print arr
-print bubble_sort(arr)
+print merge_sort(arr)
